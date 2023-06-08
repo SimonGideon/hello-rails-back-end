5cdeffbb9dc5f5a5dfaadd925411528ee37892df
SimonGideon <simongideon918@gmail.com>
2023-06-08 12:47:28 +0300

    Controler and routes

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
+  def random_greetings
+    message = Message.order("RANDOM()").first
+    render json: { greeting: message.greeting }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [] do
+        collection do
+          get "random", to: "messages#random_greetings"
+        end
+      end
+    end
+  end
 end
